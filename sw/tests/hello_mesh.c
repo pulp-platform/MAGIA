558f61210453c51bb6b520d22abe1102880abdac
Victor Isachi <victor.isachi@unibo.it>
2024-09-13 16:49:19 +0200

    Updated the test to use the parallel safe print

--- sw/tests/hello_mesh.c
+++ sw/tests/hello_mesh.c
@@ -2,7 +2,7 @@
 #include "redmule_mesh_utils.h"
 
 int main(void) {
-  printf("Hello World! it is hartid 0x%0x\n", get_hartid());
+  h_pprintf("Hello World! it is hartid "); pprintf(ds(get_hartid())); pprintln;
 
   mmio32(TEST_END_ADDR + get_hartid()) = DEFAULT_EXIT_CODE - get_hartid();
 
