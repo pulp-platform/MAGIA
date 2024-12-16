2990ca0458ced99f45f580749d950b03ea11b7b2
Victor Isachi <victor.isachi@unibo.it>
2024-12-16 19:55:02 +0100

    Parametrized inter L1 test

--- sw/tests/inter_l1_test.c
+++ sw/tests/inter_l1_test.c
@@ -1,37 +1,43 @@
 #include "redmule_mesh_utils.h"
 
+#define NUM_HARTS (4)
+
+#define MEM_OFFSET (0x1000)
+
+#define SETTLE_CYCLE (NUM_HARTS*500)
+
 int main() {
-    int32_t error = 0;
+  uint32_t error[NUM_HARTS];
+  uint32_t total_errors;
 
-    // Write the tiles ID to different L1 memory locations in other tiles
-    for(int i=0; i<4; i++) {
-        if(get_hartid() != i) {
-            *(volatile int*) (L1_BASE + i*L1_TILE_OFFSET + 0x1000 + get_hartid()*4) = (int) get_hartid();
-        }
+  // Write the tiles ID to different L1 memory locations in other tiles
+  for(int i = 0; i < NUM_HARTS; i++) {
+    if(get_hartid() != i) {
+      *(volatile int*) (L1_BASE + i*L1_TILE_OFFSET + MEM_OFFSET + get_hartid()*4) = (int) get_hartid();
     }
+  }
 
-    // Read back the values
-    for (int i=0; i<4; i++) {
-        if(get_hartid() != i) {
-            if (*(volatile int *) (L1_BASE + i*L1_TILE_OFFSET + 0x1000 + get_hartid()*4) != get_hartid()) {
-                h_pprintf("Read wrong value, expected "); pprintf(ds(get_hartid())); pprintln;
-                error++;
-            }
-        }
+  // Read back the values
+  for (int i = 0; i < NUM_HARTS; i++) {
+    if(get_hartid() != i) {
+      if (*(volatile int *) (L1_BASE + i*L1_TILE_OFFSET + MEM_OFFSET + get_hartid()*4) != get_hartid()) {
+        h_pprintf("Read wrong value, expected "); pprintf(ds(get_hartid())); pprintln;
+        error[get_hartid()]++;
+      }
     }
+  }
 
-    
-    if(error == 0) {
-        if(get_hartid() == 0) {
-            h_pprintf("TEST PASSED!!"); pprintln;
-        }
-        mmio8(TEST_END_ADDR + get_hartid()) = PASS_EXIT_CODE;
-    } else {
-        if (get_hartid() == 0) {
-            h_pprintf("TEST FAILED!!"); pprintln;
-        }
-        mmio8(TEST_END_ADDR + get_hartid()) = FAIL_EXIT_CODE;
-    }
+  wait_nop(SETTLE_CYCLE);
 
-    return 0;
+  if (get_hartid() == 0) {
+    for (int i = 0; i < NUM_HARTS; i++)
+      if (error[i]) total_errors++;
+    if (total_errors) { h_pprintf("TEST FAILED!!"); pprintln; }
+    else              { h_pprintf("TEST PASSED!!"); pprintln; }
+  } else wait_nop(SETTLE_CYCLE);
+
+  if (total_errors) mmio8(TEST_END_ADDR + get_hartid()) = FAIL_EXIT_CODE;
+  else              mmio8(TEST_END_ADDR + get_hartid()) = PASS_EXIT_CODE;         
+
+  return 0;
 }
