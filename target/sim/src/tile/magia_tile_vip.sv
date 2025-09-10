684df8e40098eefb0d7d77afc751f86940b55f75
Alessandro Nadalini <alessandro.nadalini3@unibo.it>
2025-09-10 14:54:14 +0200

    Align standalone tile simulation environment to new return routine

--- target/sim/src/tile/magia_tile_vip.sv
+++ target/sim/src/tile/magia_tile_vip.sv
@@ -169,7 +169,7 @@ module magia_tile_vip
   endtask: elf_run
 
   task automatic wait_for_eoc(output bit[31:0] exit_code);
-    while ({i_l2_mem.i_l2_mem.mem[32'hCC03_0001], i_l2_mem.i_l2_mem.mem[32'hCC03_0000]} == 0)
+    while ({i_l2_mem.mem[32'hCC03_0001][3]} == 0)
       #10000;
     exit_code = {i_l2_mem.i_l2_mem.mem[32'hCC03_0001], i_l2_mem.i_l2_mem.mem[32'hCC03_0000]};
   endtask: wait_for_eoc
