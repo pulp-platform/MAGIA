5b2f1481782f1b34852696a3d36685f73edf4db0
Alessandro Nadalini <alessandro.nadalini3@unibo.it>
2025-09-10 16:01:10 +0200

    Added missing semicolumn

--- target/sim/src/mesh/magia_tb.sv
+++ target/sim/src/mesh/magia_tb.sv
@@ -49,7 +49,7 @@ module magia_tb;
     if(exit_code != 0)
       $fatal(1, "SIMULATION FINISHED WITH EXIT CODE: %0h\n", exit_code);
     else begin
-      $display("SIMULATION FINISHED WITH EXIT CODE: %0h\n", exit_code)
+      $display("SIMULATION FINISHED WITH EXIT CODE: %0h\n", exit_code);
       $finish(0);
     end
     
