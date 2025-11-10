697de1d574d3bcd70ba42c5bd9363a09d3e6d31e
Luca Balboni <luca.balboni10@studio.unibo.it>
2025-11-10 14:32:01 +0100

    Update comments and add  mesh test event unit new

--- sw/tests/mesh_test_event_unit.c
+++ sw/tests/mesh_test_event_unit.c
@@ -55,7 +55,7 @@
 
 #define DIFF_TH (0x0011)
 
-#define USE_WFE (1)
+#define USE_WFE (0)
 
 void idma_mv_in_pure_eu(unsigned int x_dim, unsigned int y_dim, uint16_t src_data[], uint32_t dst_address) {
   uint32_t dst_addr;
@@ -158,6 +158,12 @@ void idma_mv_out_pure_eu(unsigned int x_dim, unsigned int y_dim, uint32_t src_ad
 }
 
 int main(void) {
+  // Execute test ONLY on tile 0
+  if (get_hartid() != 0) {
+    mmio16(TEST_END_ADDR + get_hartid()*2) = PASS_EXIT_CODE - get_hartid();
+    return 0;
+  }
+
   // X
   printf("Initializing X through iDMA...\n");
   idma_mv_in_pure_eu(M_SIZE, N_SIZE, x_inp, (X_BASE + get_hartid()*L1_TILE_OFFSET));
@@ -208,10 +214,13 @@ int main(void) {
 
   printf("Verifying results...\n");
   
+  // Debug: print first few values
+  printf("DEBUG: First z_oup values: 0x%0x 0x%0x 0x%0x 0x%0x\n", z_oup[0], z_oup[1], z_oup[2], z_oup[3]);
+  
   unsigned int num_errors[NUM_HARTS];
   num_errors[get_hartid()] = 0;
 
-  uint16_t computed[NUM_HARTS], expected[NUM_HARTS], diff[NUM_HARTS];
+  volatile uint16_t computed[NUM_HARTS], expected[NUM_HARTS], diff[NUM_HARTS];
   for(int i = 0; i < M_SIZE*K_SIZE; i++){
     computed[get_hartid()] = mmio16(V_BASE + get_hartid()*MHARTID_OFFSET + 2*i);
     expected[get_hartid()] = z_oup[i];
