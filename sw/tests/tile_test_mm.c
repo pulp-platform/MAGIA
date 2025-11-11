0a9205c59f74db57a8a8e927e80322608de31781
Luca Balboni <lucabalbo2001@gmail.com>
2025-11-11 17:01:56 +0100

    Update tile_test_mm.c

--- sw/tests/tile_test_mm.c
+++ sw/tests/tile_test_mm.c
@@ -15,7 +15,7 @@
  * SPDX-License-Identifier: Apache-2.0
  *
  * Authors: Luca Balboni <luca.balboni10@studio.unibo.it>
- *         Based on tile_test.c by Victor Isachi
+ *          Based on tile_test.c by Victor Isachi
  * 
  * MAGIA Tile Test - Memory Mapped Version
  */
