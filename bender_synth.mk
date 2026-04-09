c7c9234e3fea4f60bdaa2b4e72d2033cf9da86be
Luca Balboni <lucabalbo2001@gmail.com>
2026-04-09 14:36:43 +0200

    Deleted the standalone_tile target from bende_synth.mk

--- bender_synth.mk
+++ bender_synth.mk
@@ -19,10 +19,6 @@ synth_targs += -t asic
 synth_targs += -t synopsys
 synth_targs += -t spatz
 
-ifeq ($(mesh_dv), 0)
-synth_defs += -D TARGET_STANDALONE_TILE
-endif
-
 synth_defs += -D REDMULE_HWPE_SYNTH
 synth_defs += -D SYNTHESIS
 synth_defs += -D TARGET_SPATZ
