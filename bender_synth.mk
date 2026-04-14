a5015141e02ff8f70c958fdc34d7a4ffa6ce8550
Luca Balboni <lucabalbo2001@gmail.com>
2026-04-14 16:26:50 +0200

    Added Synth defs for Spatz CC

--- bender_synth.mk
+++ bender_synth.mk
@@ -22,3 +22,14 @@ synth_targs += -t spatz
 synth_defs += -D REDMULE_HWPE_SYNTH
 synth_defs += -D SYNTHESIS
 synth_defs += -D TARGET_SPATZ
+synth_defs += -D SPATZ_RVD=$(SPATZ_RVD)
+synth_defs += -D SPATZ_VLEN=$(SPATZ_VLEN)
+synth_defs += -D SPATZ_NRVREG=$(SPATZ_NRVREG)
+synth_defs += -D SPATZ_NR_VRF_BANKS=$(SPATZ_NR_VRF_BANKS)
+synth_defs += -D SPATZ_N_IPU=$(SPATZ_N_IPU)
+synth_defs += -D SPATZ_N_FPU=$(SPATZ_N_FPU)
+synth_defs += -D SPATZ_NR_PARALLEL_INSTR=$(SPATZ_NR_PARALLEL_INSTR)
+synth_defs += -D SPATZ_XDIVSQRT=$(SPATZ_XDIVSQRT)
+synth_defs += -D SPATZ_XDMA=$(SPATZ_XDMA)
+synth_defs += -D SPATZ_RVF=$(SPATZ_RVF)
+synth_defs += -D SPATZ_RVV=$(SPATZ_RVV)
