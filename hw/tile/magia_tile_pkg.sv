057d57554d53c9adaceef560fa47bbd9f5abf0a1
Alessandro Nadalini <alessandro.nadalini3@unibo.it>
2026-05-12 10:11:08 +0200

    cluster_icache dependency update
    - Updated the cluster_icache to v0.2.0
    - Added the I$ wrapper since it was removed from the cluster_icache repository

--- hw/tile/magia_tile_pkg.sv
+++ hw/tile/magia_tile_pkg.sv
@@ -475,7 +475,7 @@ package magia_tile_pkg;
   parameter int unsigned L0_LINE_COUNT  = 32;                                           // i$ L0 Cache Line Count
   parameter int unsigned LINE_WIDTH     = 128;                                          // i$ Cache Line Width; >= 64
   parameter int unsigned LINE_COUNT     = 32;                                           // i$ The number of cache lines per set. Power of two; >= 2.
-  parameter int unsigned SET_COUNT      = 32;                                           // i$ The set associativity of the cache. Power of two; >= 1.
+  parameter int unsigned WAY_COUNT      = 32;                                           // i$ The set associativity of the cache. Power of two; >= 1.
   parameter int unsigned L0_PARITY_W    = 0;                                            // i$ Parity of the L0 cache
   parameter int unsigned L1_PARITY_W    = L0_PARITY_W;                                  // i$ Parity of the L1 cache
   parameter int unsigned FETCH_AW       = magia_pkg::ADDR_W;                            // i$ Fetch interface address width. Same as FETCH_AW; >= 1.
