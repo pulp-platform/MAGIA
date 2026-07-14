656ab17174656bf7f18c4d566e31f5aee5d37a1f
Alessandro Nadalini <alessandro.nadalini3@unibo.it>
2026-07-14 11:30:25 +0200

    Removed exports related to local environment in setup.sh

--- setup_env.sh
+++ setup_env.sh
@@ -9,10 +9,6 @@ echo "Exporting bender path to $BENDER_DIR"
 export PATH=$BENDER_DIR:$PATH
 unset BENDER_DIR
 echo "Exporting SDK and GCC Toolchain paths"
-export PATH=/usr/pack/pulpsdk-1.0-kgf/artifactory/pulp-sdk-release/pkg/pulp_riscv_gcc/1.0.16/bin:$PATH
-export PATH=/usr/pack/gcc-5.2.0-af/x86_64-rhe6-linux/bin:$PATH
-export PATH=/usr/local/anaconda3-2023.07/condabin:$PATH
-export PATH=/home/visachi/.local/bin:$PATH
 export XLEN=32
 if [[ "$core" == "CV32E40P" ]] || [[ "$core" == "RI5CY" ]]; then
   echo "Exporting ISA extentions: I, M, C, PULP"
