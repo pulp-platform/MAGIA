e22c1b386dee91eadd750e5566d1df3fe1342435
Luca Balboni <lucabalbo2001@gmail.com>
2026-08-06 13:47:07 +0200

    zfinx parametrized and fixed crt0.s (for CV32E40X zfinx is always = 0, it has issue for =1 with fpu_ss)

--- sw/tests/hetero_mesh_test/main.c
+++ sw/tests/hetero_mesh_test/main.c
@@ -14,7 +14,7 @@
  * limitations under the License.
  * SPDX-License-Identifier: Apache-2.0
  * 
- * Authors: Luca Balboni <luc.balboni@chips.it>
+ * Authors: Luca Balboni <luca.balboni@chips.it>
  * 
  *  Test for the heterogeneous mesh
  */
