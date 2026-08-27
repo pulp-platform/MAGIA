de84d5da59160504206be5a26b307d19143c471b
Luca Balboni <lucabalbo2001@gmail.com>
2026-08-27 12:15:18 +0200

    Magia HCI Interconnect Reworked

--- hw/tile/magia_tile_pkg.sv
+++ hw/tile/magia_tile_pkg.sv
@@ -234,6 +234,8 @@ package magia_tile_pkg;
   parameter int unsigned UW_LIC  = magia_pkg::USR_W;                                    // User Width for Log Interconnect
   localparam int unsigned SW_LIC = DW_LIC/BW_LIC;                                       // Strobe Width for Log Interconnect
   parameter int unsigned TS_BIT  = 21;                                                  // TEST_SET_BIT (for Log Interconnect)
+  parameter int unsigned HCI_QOS_NUM = 1;                                               // QoS numerator for wide<narrow arbiter
+  parameter int unsigned HCI_QOS_DEN = 2;                                               // QoS denominator for wide<narrow arbiter
   parameter int unsigned IW      = N_HWPE+N_CORE+N_DMA+N_EXT;                           // ID Width HCI
   parameter int unsigned EXPFIFO = 0;                                                   // FIFO Depth for HWPE Interconnect
   parameter int unsigned DWH     = magia_pkg::MagiaRedMuleDefaultCfg.Height *
